43b1b91cdeed8762429be28e3fc968d01cb150be
Kaushik7984 <kaushik7984@gmail.com>
2024-07-11 09:39:02 +0530

    edit file

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from './Home.module.css'
 
 function HomePage() {
   return (
-    <>
-    <h1>My home Page</h1>
-    <p>go to <Link to='/events'>the list of events</Link> </p>
-    </>
-  )
+    <div className={classes.container}>
+      <h1>This is Home Page</h1>
+      <h1>
+        Go to <Link to="/events">Event Page</Link>{" "}
+      </h1>
+      <p>
+        
+        Offers an easy way to manage your events likes (Add Event, Edit Event,
+        and Delete Event).
+      </p>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
